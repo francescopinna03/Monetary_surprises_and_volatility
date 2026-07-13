0a13ac7df617fe4dc6aae4cecd409298a884b39b
Francesco Pinna <francescopinna03@gmail.com>
2026-07-13 16:46:37 +0200

    Update Shock_purification_models.m

--- Shock_purification_models.m
+++ Shock_purification_models.m
@@ -33,7 +33,7 @@
 
 clear; clc;
 
-projectRoot = get_project_root();
+projectRoot = Get_project_root();
 
 analysisDir = fullfile(projectRoot, 'Output', 'analysis');
 eventFile = fullfile(analysisDir, 'event_state_panel.csv');
