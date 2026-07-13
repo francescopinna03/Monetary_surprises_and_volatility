c094b511e0628826bce251f84baef09e9500e49f
Francesco Pinna <francescopinna03@gmail.com>
2026-07-13 16:45:30 +0200

    Update PR_bar_panel.m

--- PR_bar_panel.m
+++ PR_bar_panel.m
@@ -26,7 +26,7 @@
 
 clear; clc;
 
-projectRoot = get_project_root();
+projectRoot = Get_project_root();
 
 analysisDir = fullfile(projectRoot, 'Output', 'analysis');
 cleanDir = fullfile(projectRoot, 'Output', 'cleaned');
